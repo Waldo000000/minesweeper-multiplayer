bba1f405e6aa0c6cbea5c849344ff3a8f5583789
Roy Wallace <waldo000000@gmail.com>
2023-12-13 19:06:27 +1000

    Add "New Game" button

--- app/game/ui/game.tsx
+++ app/game/ui/game.tsx
@@ -13,17 +13,26 @@ const Game: React.FC<GameProps> = ({ nRows, nCols, nMines }) => {
     const [minesweeperGame, setMinesweeperGame] = useState<MinesweeperGame | null>(null);
     const [state, setState] = useState<State | null>(null);
 
-    useEffect(() => {
-        const game = new MinesweeperGame(nRows, nCols, nMines, (state) => setState({...state}));
-         
-        setMinesweeperGame(game);
-    }, [nRows, nCols, nMines]);
+    const newGame = () => new MinesweeperGame(nRows, nCols, nMines, (state) => setState({ ...state }));
 
-    return <Grid
-        board={state?.board ?? []}
-        onCellClick={minesweeperGame?.revealCell || (() => { })}
-        onCellRightClick={minesweeperGame?.flagCell || (() => { })}
-    />
+    useEffect(() => setMinesweeperGame(newGame()), [nRows, nCols, nMines]);
+
+    const handleReset = () => setMinesweeperGame(newGame());
+
+    return (
+        <div>
+            <Grid
+                board={state?.board ?? []}
+                onCellClick={minesweeperGame?.revealCell || (() => { })}
+                onCellRightClick={minesweeperGame?.flagCell || (() => { })}
+            />
+            <button
+                onClick={handleReset}
+                className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                New Game
+            </button>
+        </div >
+    )
 }
 
 export default Game;
